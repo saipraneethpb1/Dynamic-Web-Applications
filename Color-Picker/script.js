3b2e538ceb04037663a0468a23aea23882cce75e
saipraneethpb1 <praneethpb2003@gmail.com>
2026-08-13 15:18:34 +0530

    Color picker with js

---
+++ Color-Picker/script.js
@@ -0,0 +1,22 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+
+function funb1() {
+    colorPickerContainer.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCode.textContent = "#e0e0e0";
+}
+
+function funb2() {
+    colorPickerContainer.style.backgroundColor = "#6fcf97";
+    selectedColorHexCode.textContent = "#6fcf97";
+}
+
+function funb3() {
+    colorPickerContainer.style.backgroundColor = "#56ccf2";
+    selectedColorHexCode.textContent = "#56ccf2";
+}
+
+function funb4() {
+    colorPickerContainer.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCode.textContent = "#bb6bd9";
+}
