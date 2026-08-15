147b631b741e4bf8bf4d3aaf22fe39b485b504fc
saipraneethpb1 <praneethpb2003@gmail.com>
2026-08-15 20:47:06 +0530

    seasons-switcher: js

---
+++ Seasons-Switcher/script.js
@@ -0,0 +1,22 @@
+let smallImgEl = document.getElementById("seasonSmallImage");
+let mediumImgEl = document.getElementById("seasonMediumImage");
+
+function springFn() {
+    smallImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    mediumImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
+
+function summerFn() {
+    smallImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+    mediumImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+}
+
+function autumnFn() {
+    smallImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+    mediumImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+}
+
+function winterFn() {
+    smallImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+    mediumImgEl.src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+}
